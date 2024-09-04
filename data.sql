6f36e5aba035d0b9501a9f3dc726e90e26bc4049
ronnie1253 <syunmandal3103@gmail.com>
2024-09-05 02:14:55 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO car (name, type, price) VALUES ('Toyota Corolla', 'Sedan', 20000);
+INSERT INTO car (name, type, price) VALUES ('Honda Civic', 'Sedan', 22000);
+INSERT INTO car (name, type, price) VALUES ('Ford Mustang', 'Coupe', 26000);
+INSERT INTO car (name, type, price) VALUES ('Jeep Wrangler', 'SUV', 30000);
+INSERT INTO car (name, type, price) VALUES ('Chevrolet Tahoe', 'SUV', 40000);
