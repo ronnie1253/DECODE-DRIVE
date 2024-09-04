a9fe89f995ce892ead4360c82e80187f540a2254
ronnie1253 <syunmandal3103@gmail.com>
2024-09-05 02:19:49 +0530

    Update CarController.java

--- CarController.java
+++ CarController.java
@@ -3,20 +3,28 @@ package com.example.carsuggestion.controller;
 import com.example.carsuggestion.model.Car;
 import com.example.carsuggestion.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class CarController {
 
     @Autowired
     private CarService carService;
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
     @GetMapping("/suggest-cars")
-    public List<Car> suggestCars(@RequestParam String type, @RequestParam double budget) {
-        return carService.suggestCars(type, budget);
+    public String suggestCars(@RequestParam String type, @RequestParam double budget, Model model) {
+        List<Car> cars = carService.suggestCars(type, budget);
+        model.addAttribute("cars", cars);
+        return "index";
     }
 }
